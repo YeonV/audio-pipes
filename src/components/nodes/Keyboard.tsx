a97e6b67c6fa7c05e106b9be95607ed3904a2638
YeonV <trusted.yeon@gmail.com>
2022-02-14 13:18:38 +0100

    Add keypress to Keyboard

--- src/components/nodes/Keyboard.tsx
+++ src/components/nodes/Keyboard.tsx
@@ -30,6 +30,82 @@ function Keyboard({ data, id, type }: NodeProps) {
   const playNote = useCallback(() => void (gateNode.offset.value = 1), [gateNode]);
   const stopNote = useCallback(() => void (gateNode.offset.value = 0), [gateNode]);
 
+  // const isLargeStep = useKeyPress(1);
+
+  function onKeyDown({ key }: React.KeyboardEvent<HTMLInputElement>) {
+    switch (key) {
+      case "a":
+        setNote(1,9)        
+        playNote()
+        break;
+      case "w":
+        setNote(1,10)        
+        playNote()
+        break;
+      case "s":
+        setNote(1,11)        
+        playNote()
+        break;
+      case "d":
+        setNote(2,0)        
+        playNote()
+        break;
+      case "r":
+        setNote(2,1)        
+        playNote()
+        break;
+      case "f":
+        setNote(2,2)        
+        playNote()
+        break;
+      case "t":
+        setNote(2,3)        
+        playNote()
+        break;
+      case "g":
+        setNote(2,4)        
+        playNote()
+        break;
+      case "h":
+        setNote(2,5)        
+        playNote()
+        break;
+      case "u":
+        setNote(2,6)        
+        playNote()
+        break;
+      case "j":
+        setNote(2,7)        
+        playNote()
+        break;
+      case "i":
+        setNote(2,8)        
+        playNote()
+        break;
+      case "k":
+        setNote(2,9)        
+        playNote()
+        break;
+      case "o":
+        setNote(2,10)        
+        playNote()
+        break;
+      case "l":
+        setNote(2,11)        
+        playNote()
+        break;
+      case "ö":
+        setNote(3,0)        
+        playNote()
+        break;    
+      default:
+        break;
+    }
+  }
+  function onKeyUp({ key }: React.KeyboardEvent<HTMLInputElement>) {
+    stopNote()
+  }
+
   // AudioNode
   useNode(
     id,
@@ -46,14 +122,14 @@ function Keyboard({ data, id, type }: NodeProps) {
     <Node id={id} outputs={["frequency", "gate"]} title={`Keyboard`} type={type}>
       <div className="customNode_editor nodrag">
         <div className="customNode_item">
-          <div className="keyboard" onMouseDown={playNote} onMouseLeave={stopNote} onMouseUp={stopNote}>
+          <div className="keyboard" onMouseDown={playNote} onKeyDown={onKeyDown} onKeyUp={onKeyUp} onMouseLeave={stopNote} onMouseUp={stopNote}>
             {Array(keys)
               .fill(null)
               .map((_, keyIndex) => {
                 const keyTwelfth = (((keyIndex + keyTwelfthOffset) % 12) + 12) % 12;
                 const keyOctave = octave + Math.floor((keyIndex + keyTwelfthOffset) / 12);
                 const keyClassName = [1, 3, 6, 8, 10].includes(keyTwelfth) ? keyBlack : keyWhite;
-
+                console.log(keyOctave, keyTwelfth)
                 return (
                   <button
                     className={keyClassName}
